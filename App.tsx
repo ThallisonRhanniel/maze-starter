4933382b041ac11629990410eda160f3baf7bbde
thallisonrhannielgarcialima <thallisonlima@hotmail.com>
2023-06-28 21:14:18 -0300

    Criando um componente de texto

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { ThemeProvider } from 'styled-components';
+import Text from "./src/common/components/Text";
 import { defaultTheme } from './src/common/constants/styles/theme/defaultTheme';
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!2</Text>
+        <Text size={40} color="secondary">
+          Meu componente de tex2
+        </Text>
         <StatusBar style="auto" />
       </View>
     </ThemeProvider>
