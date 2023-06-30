2e6833d6a61ae33359cc0bd67442fdbd427aedf4
thallisonrhannielgarcialima <thallisonlima@hotmail.com>
2023-06-29 21:29:49 -0300

    Criando um componente para adicionar espaçamento

--- App.tsx
+++ App.tsx
@@ -2,14 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import Text from "./src/common/components/Text";
+import Spacer from "./src/common/components/Spacer";
 import { defaultTheme } from './src/common/constants/styles/theme/defaultTheme';
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <View style={styles.container}>
-        <Text size={40} color="secondary">
-          Meu componente de tex2
+        <Text size={30} color="secondary">
+          1 Meu componente de text
+        </Text>
+        <Spacer height={30} />
+        <Text size={30} color="secondary">
+          2 Meu componente de text
         </Text>
         <StatusBar style="auto" />
       </View>
